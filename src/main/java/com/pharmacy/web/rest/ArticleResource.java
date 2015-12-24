f9b6a614e74e389f52b3b8af6b4829901153a6df
Alexander Popow <apopow@gmail.com>
2015-12-24 10:23:41 +0100

    add

--- src/main/java/com/pharmacy/web/rest/ArticleResource.java
+++ src/main/java/com/pharmacy/web/rest/ArticleResource.java
@@ -133,6 +133,9 @@ public class ArticleResource {
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
     public List<Article> searchArticles(@PathVariable String query) {
+
+        Iterable<Article> test = articleSearchRepository.findAll();
+
         return StreamSupport
             .stream(articleSearchRepository.search(queryStringQuery(query)).spliterator(), false)
             .collect(Collectors.toList());
